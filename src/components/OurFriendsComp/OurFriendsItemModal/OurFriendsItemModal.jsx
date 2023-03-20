7ac5960a3fb2e060ba01c0a15866ffaaffa63ccf
JuliaGolban <julia.j.shcherban@gmail.com>
2023-03-20 22:56:17 +0200

    add markup modal with times for friends

--- src/components/OurFriendsComp/OurFriendsItemModal/OurFriendsItemModal.jsx
+++ src/components/OurFriendsComp/OurFriendsItemModal/OurFriendsItemModal.jsx
@@ -1,38 +1,79 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
-import {  Day, Time } from './OurFriendsItemModal.styled';//Modal, Table,
+import { Modal, Table, Line, Day, Time } from './OurFriendsItemModal.styled';
 
-export const OurFriendsItemModal = workDays => {
-  // const { isOpen, from, to } = workDays;
-  // const workTime = [
-  //   {
-  //     MN: workDays[0],
-  //     TU: workDays[1],
-  //     WE: workDays[2],
-  //     TH: workDays[3],
-  //     FR: workDays[4],
-  //     SA: workDays[5],
-  //     SU: workDays[6],
-  //   },
-  // ];
+const modalRoot = document.querySelector('#worktime-root');
 
-  // const handleOpenTime = () => {};
+export const OurFriendsItemModal = ({ onClose, workDays }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') onClose();
+    };
 
-  // const handleToogleModal = () => {};
+    window.addEventListener('keydown', handleKeyDown);
 
-  return (
-    <div>
-      <Day></Day>
-      <Time></Time>
-    </div>
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  const workTime = {
+    MN: workDays[0].isOpen ? workDays[0].from + ' - ' + workDays[0].to : '',
+    TU: workDays[1].isOpen ? workDays[1].from + ' - ' + workDays[1].to : '',
+    WE: workDays[2].isOpen ? workDays[2].from + ' - ' + workDays[2].to : '',
+    TH: workDays[3].isOpen ? workDays[3].from + ' - ' + workDays[3].to : '',
+    FR: workDays[4].isOpen ? workDays[4].from + ' - ' + workDays[4].to : '',
+    SA: workDays[5].isOpen ? workDays[5].from + ' - ' + workDays[5].to : '',
+    SU: workDays[6].isOpen ? workDays[6].from + ' - ' + workDays[6].to : '',
+  };
+
+  return createPortal(
+    <Modal onClick={() => onClose()}>
+      <table>
+        <Table>
+          <Line>
+            <Day>MN</Day>
+            <Time>{workTime.MN}</Time>
+          </Line>
+          <Line>
+            <Day>TU</Day>
+            <Time>{workTime.TU}</Time>
+          </Line>
+          <Line>
+            <Day>WE</Day>
+            <Time>{workTime.WE}</Time>
+          </Line>
+          <Line>
+            <Day>TH</Day>
+            <Time>{workTime.TH}</Time>
+          </Line>
+          <Line>
+            <Day>FR</Day>
+            <Time>{workTime.FR}</Time>
+          </Line>
+          <Line>
+            <Day>SA</Day>
+            <Time>{workTime.SA}</Time>
+          </Line>
+          <Line>
+            <Day>SU</Day>
+            <Time>{workTime.SU}</Time>
+          </Line>
+        </Table>
+      </table>
+    </Modal>,
+    modalRoot,
   );
 };
 
 OurFriendsItemModal.propTypes = {
   workDays: PropTypes.arrayOf(
     PropTypes.shape({
-      isOpen: PropTypes.bool.isRequired,
+      isOpen: PropTypes.bool,
       from: PropTypes.string,
       to: PropTypes.string,
-    }),
+    }).isRequired,
   ),
+  onClose: PropTypes.func.isRequired,
 };
