fb5d7bef8b4c6dda714a6f87be90b559e445e701
RudenkoSerhii <rudenko.sergey@ukr.net>
2023-03-20 11:01:10 +0200

    add toggle modal

--- src/components/NoticesComp/AddNoticeButton/AddNoticeButton.jsx
+++ src/components/NoticesComp/AddNoticeButton/AddNoticeButton.jsx
@@ -1,10 +1,20 @@
 import { AddNoticeModal } from "./AddNoticeModal/AddNoticeModal";
+import { Button, PlusIcon } from "./AddNoticeButton.styled";
+import { useSelector } from 'react-redux';
+import Notiflix from "notiflix";
 
 export const AddNoticeButton = () => {
+    const isLogin = useSelector(selectIsStateChange);
+
+  const [showModalAddNotice, setShowModalAddNotice] = useState(false);
+
+  const toggleModalAddNotice = () => {
+isLogin ? setShowModalAddNotice(!showModalAddNotice) : Notiflix.Notify.warning('You must be loggined!');
+  };
     return (
         <div>
-            <h1>Add Notice Button</h1>
-            <AddNoticeModal />
+            <Button onClick={toggleModalAddNotice}><PlusIcon/>Add pet</Button>
+            {(showModalAddNotice) && <AddNoticeModal onClose={toggleModalAddNotice}/>}
         </div>
     );
 }
