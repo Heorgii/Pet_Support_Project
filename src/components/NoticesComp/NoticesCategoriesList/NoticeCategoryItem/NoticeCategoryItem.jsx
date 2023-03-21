e22c572e9fc84f5089b2bde8c2a344982db19e29
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-03-21 18:57:44 +0200

    feature-notice

--- src/components/NoticesComp/NoticesCategoriesList/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticesComp/NoticesCategoriesList/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,72 +1,55 @@
-import data from './pets.json';
-import ReactDOM from 'react-dom';
-import { MdClose } from 'react-icons/md';
-import { closeModalWindow } from '../../../../hooks/modalWindow';
-import heard from '../../../../images/svg/heard.png';
+import akar_icons_heart from '../../../../images/svg/akar-icons_heart.svg';
+import delBack from '../../../../images/svg/icon_delete.svg';
 import no_Photo from '../../../../images/No-image-available.webp';
 import {
   NoticesContainerItem,
-  ContainerCloseModal,
   ContainerInfo,
   ImgItem,
   ContainerStatus,
   NoticeItemTitle,
-  BtnContact,
-  BtnAddFavorits,
-  BackDrop,
+  BtnLearnMore,
+  BtnDelete,
+  ItemContainer,
   TdTable,
   TdTable2,
   Table,
-  Comments,
-  ContainerComments,
-  NoticeContainerButtom,
+  NoticeContainerButton,
+  BtnForFavorite,
 } from './NoticeCategoryItem.styled';
 
-export const NoticesCategoriesItem = () => {
-  return ReactDOM.createPortal(
-    <BackDrop onClick={e => closeModalWindow(e)}>
-      <NoticesContainerItem onClick={e => e.stopPropagation()}>
-        <div style={{ display: 'flex', justifyContent: 'end' }}>
-          <ContainerCloseModal onClick={e => closeModalWindow(e)}>
-            <MdClose style={{ width: '15.5px', height: '15.5px' }} />
-          </ContainerCloseModal>
-        </div>
+export const NoticesCategoriesItem = ({ data }) => {
+  return (
+    <ItemContainer>
+      <NoticesContainerItem>
+        <div style={{ display: 'flex', justifyContent: 'end' }}></div>
         <ContainerInfo>
           <ContainerStatus>{data.status}</ContainerStatus>
+          <BtnForFavorite>
+            <img src={akar_icons_heart} alt="Add to favorite" />
+          </BtnForFavorite>
           <ImgItem src={no_Photo} />
           <div>
-            <NoticeItemTitle>Сute dog looking for a home</NoticeItemTitle>
+            <NoticeItemTitle>Сute pet looking for a home</NoticeItemTitle>
             <div>
               <Table>
                 <thead></thead>
                 <tbody>
                   <tr>
-                    <TdTable>Name:</TdTable>
-                    <TdTable2>{data.name}</TdTable2>
-                  </tr>
-                  <tr>
-                    <TdTable>Birthday:</TdTable>
-                    <TdTable2>{data.date}</TdTable2>
-                  </tr>
-                  <tr>
                     <TdTable>Breed:</TdTable>
                     <TdTable2>{data.breed}</TdTable2>
                   </tr>
                   <tr>
-                    <TdTable>Location:</TdTable>
+                    <TdTable>Place:</TdTable>
                     <TdTable2>{data.location}</TdTable2>
                   </tr>
                   <tr>
-                    <TdTable>The sex:</TdTable>
-                    <TdTable2>{data.sex}</TdTable2>
-                  </tr>
-                  <tr>
-                    <TdTable>Email:</TdTable>
-                    <TdTable2>{data.email}</TdTable2>
-                  </tr>
-                  <tr>
-                    <TdTable>Phone:</TdTable>
-                    <TdTable2>{data.phone}</TdTable2>
+                    <TdTable>Age:</TdTable>
+                    <TdTable2>
+                      {Math.round(
+                        (Date.now() - Date.parse(data.date)) / 31536000 / 1000,
+                      )}{' '}
+                      years
+                    </TdTable2>
                   </tr>
                 </tbody>
                 <tfoot></tfoot>
@@ -74,19 +57,14 @@ export const NoticesCategoriesItem = () => {
             </div>
           </div>
         </ContainerInfo>
-        <ContainerComments>
-          <span style={{ fontWeight: 600 }}>Comments: </span>
-          {data.comments}
-          <Comments></Comments>
-        </ContainerComments>
-        <NoticeContainerButtom>
-          <BtnContact>Contacts</BtnContact>
-          <BtnAddFavorits>
-            Add to <img src={heard} alt="heard" />
-          </BtnAddFavorits>
-        </NoticeContainerButtom>
+        <NoticeContainerButton>
+          <BtnLearnMore>Learn more</BtnLearnMore>
+          <BtnDelete>
+            Delete{' '}
+            <img src={delBack} alt="heard" style={{ marginLeft: '12px' }} />
+          </BtnDelete>
+        </NoticeContainerButton>
       </NoticesContainerItem>
-    </BackDrop>,
-    document.querySelector('#popup-root'),
+    </ItemContainer>
   );
 };
--- src/components/NoticesComp/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/NoticesComp/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,10 +1,16 @@
-import { NoticesCategoriesItem } from "./NoticeCategoryItem/NoticeCategoryItem";
+import { NoticesCategoriesItem } from './NoticeCategoryItem/NoticeCategoryItem';
+import { ModalNotices } from '../ModalNotice/ModalNotice';
+import data from './pets.json';
+import { v4 as uuidv4 } from 'uuid';
 
 export const NoticesCategoriesList = () => {
-    return (
-        <div>
-            <h1>Notices Categories List</h1>
-            <NoticesCategoriesItem />
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Notices Categories List</h1>
+      {data.map(key => (
+        <NoticesCategoriesItem data={key} key={uuidv4()} />
+      ))}
+      <ModalNotices />
+    </div>
+  );
+};
