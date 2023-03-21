5e405d845793ffe9d6404eddf6ca75f2f46398d6
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-03-21 12:59:27 +0200

    add styled for notice item modal

--- src/components/NoticesComp/NoticesCategoriesList/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticesComp/NoticesCategoriesList/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -3,44 +3,88 @@ import ReactDOM from 'react-dom';
 import { MdClose } from 'react-icons/md';
 import { closeModalWindow } from '../../../../hooks/modalWindow';
 import heard from '../../../../images/svg/heard.png';
+import no_Photo from '../../../../images/No-image-available.webp';
 import {
   NoticesContainerItem,
   ContainerCloseModal,
+  ContainerInfo,
   ImgItem,
   ContainerStatus,
   NoticeItemTitle,
   BtnContact,
   BtnAddFavorits,
   BackDrop,
+  TdTable,
+  TdTable2,
+  Table,
+  Comments,
+  ContainerComments,
+  NoticeContainerButtom,
 } from './NoticeCategoryItem.styled';
 
 export const NoticesCategoriesItem = () => {
-  const key = Object.keys(data);
   return ReactDOM.createPortal(
     <BackDrop onClick={e => closeModalWindow(e)}>
       <NoticesContainerItem onClick={e => e.stopPropagation()}>
-        <ContainerCloseModal onClick={e => closeModalWindow(e)}>
-          <MdClose style={{ width: '15.5px', height: '15.5px' }} />
-        </ContainerCloseModal>
-        <ContainerStatus>{data.status}</ContainerStatus>
-        <ImgItem />
-        <NoticeItemTitle>Сute dog looking for a home</NoticeItemTitle>
-        <table>
-          <thead></thead>
-          <tbody>
-            {key.map(item => (
-              <tr key={item}>
-                <td>{item}</td>
-                <td key={data[item]}>{data[item]}</td>
-              </tr>
-            ))}
-          </tbody>
-          <tfoot></tfoot>
-        </table>
-        <BtnContact>Contacts</BtnContact>
-        <BtnAddFavorits>
-          Add to <img src={heard} alt="heard" />
-        </BtnAddFavorits>
+        <div style={{ display: 'flex', justifyContent: 'end' }}>
+          <ContainerCloseModal onClick={e => closeModalWindow(e)}>
+            <MdClose style={{ width: '15.5px', height: '15.5px' }} />
+          </ContainerCloseModal>
+        </div>
+        <ContainerInfo>
+          <ContainerStatus>{data.status}</ContainerStatus>
+          <ImgItem src={no_Photo} />
+          <div>
+            <NoticeItemTitle>Сute dog looking for a home</NoticeItemTitle>
+            <div>
+              <Table>
+                <thead></thead>
+                <tbody>
+                  <tr>
+                    <TdTable>Name:</TdTable>
+                    <TdTable2>{data.name}</TdTable2>
+                  </tr>
+                  <tr>
+                    <TdTable>Birthday:</TdTable>
+                    <TdTable2>{data.date}</TdTable2>
+                  </tr>
+                  <tr>
+                    <TdTable>Breed:</TdTable>
+                    <TdTable2>{data.breed}</TdTable2>
+                  </tr>
+                  <tr>
+                    <TdTable>Location:</TdTable>
+                    <TdTable2>{data.location}</TdTable2>
+                  </tr>
+                  <tr>
+                    <TdTable>The sex:</TdTable>
+                    <TdTable2>{data.sex}</TdTable2>
+                  </tr>
+                  <tr>
+                    <TdTable>Email:</TdTable>
+                    <TdTable2>{data.email}</TdTable2>
+                  </tr>
+                  <tr>
+                    <TdTable>Phone:</TdTable>
+                    <TdTable2>{data.phone}</TdTable2>
+                  </tr>
+                </tbody>
+                <tfoot></tfoot>
+              </Table>
+            </div>
+          </div>
+        </ContainerInfo>
+        <ContainerComments>
+          <span style={{ fontWeight: 600 }}>Comments: </span>
+          {data.comments}
+          <Comments></Comments>
+        </ContainerComments>
+        <NoticeContainerButtom>
+          <BtnContact>Contacts</BtnContact>
+          <BtnAddFavorits>
+            Add to <img src={heard} alt="heard" />
+          </BtnAddFavorits>
+        </NoticeContainerButtom>
       </NoticesContainerItem>
     </BackDrop>,
     document.querySelector('#popup-root'),
