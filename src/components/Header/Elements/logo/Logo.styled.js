6f1d61b5ece525563eff4e004d2c75e83730c367
Yurii Senenko <yurkosenenko@gmail.com>
2023-03-22 14:37:01 +0200

    Fix згідно з ТЗ

--- src/components/Header/Elements/logo/Logo.styled.js
+++ src/components/Header/Elements/logo/Logo.styled.js
@@ -1,7 +1,7 @@
 import { baseColor } from 'components/baseStyles/Variables.styled';
-
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
-const Text = styled.p`
+const Text = styled(NavLink)`
   font-family: 'Poppins';
   font-weight: 700;
   font-style: normal;
@@ -9,6 +9,7 @@ const Text = styled.p`
   line-height: 42px;
   letter-spacing: 0.07em;
   color: ${baseColor.colors.blackText};
+  text-decoration: none;
   @media screen and (min-width: 768px) {
     font-size: 32px;
     line-height: 48px;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,13 @@
-import { AuthNav } from './AuthNav/AuthNav';
 import { Logo } from './Elements/logo/Logo';
-import { Menu } from './Elements/menu/Menu';
-import {
-  HeaderContainer,
-  LeftContainer,
-  RightContainer,
-} from './Header.styled';
-import { NavLink } from 'react-router-dom';
-import { UserNav } from './UserNav/UserNav';
+import { Navigation } from 'components/Header/Navigation/Navigation';
+
+import { HeaderContainer } from './Header.styled';
 
 export const Header = () => {
   return (
     <HeaderContainer>
-      <LeftContainer>
-        <NavLink to="/" style={{ textDecoration: 'none' }}>
-          <Logo />
-        </NavLink>
-        <UserNav></UserNav>
-      </LeftContainer>
-      <RightContainer>
-        <AuthNav></AuthNav>
-        <Menu />
-      </RightContainer>
-
-      {/* {isLoggedIn ? <UserNav /> : <AuthNav />} */}
+      <Logo />
+      <Navigation />
     </HeaderContainer>
   );
 };
