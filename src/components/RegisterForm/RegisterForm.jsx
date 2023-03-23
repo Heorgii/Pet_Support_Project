d5c4da853740e49b7881236b026ac468d3eadfb3
Roman Kashyrin <103502911+RomanKashyrin@users.noreply.github.com>
2023-03-23 13:10:58 +0200

    finish

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,115 +1,293 @@
-import React, { useState } from 'react';
-import { FaUser, FaEnvelope, FaLock, FaUnlockAlt } from 'react-icons/fa';
-import { useDispatch } from 'react-redux';
-import { authSignUpUser } from 'redux/auth/operations';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useFormik, Formik } from 'formik';
+import { object, string, ref } from 'yup';
+// import Spinner from '../Spinner';
+import { ImEye, ImEyeBlocked } from 'react-icons/im';
 
-import css from './RegisterForm.module.css';
+import { selectIsLoading } from '../../redux/auth/selectors';
+import { register } from '../../redux/auth/operations';
 
-function Register() {
+// import {
+//   emailRegex,
+//   phoneRegexp,
+//   cityRegexp,
+//   userNameRegexp,
+//   passwordRegexp,
+// } from '../../helpers/regExpsHelpers';
+
+import {
+  FormRegister,
+  FormContainer,
+  Input,
+  Button,
+  Title,
+  ErrBox,
+  BackButton,
+  PhoneInput,
+  ShowPassword,
+  StyledLink,
+  BoxText,
+  SpinerWrapper,
+} from './RegisterForm.styled';
+
+
+
+// const emailRegex = /^[^-][a-zA-Z0-9_.-]{1,64}@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+const phoneNumberMask = [
+  '+',
+  /\d/,
+  /\d/,
+  /\d/,
+
+  /\d/,
+  /\d/,
+
+  /\d/,
+  /\d/,
+  /\d/,
+
+  /\d/,
+  /\d/,
+
+  /\d/,
+  /\d/,
+];
+
+const registerSchema = object().shape({
+  password: string()
+    .min(7, 'Password must be at least 7 characters')
+    .max(32, 'Password must be at most 32 characters')
+    .matches( 'Must not contain spaces')
+    .required('Password is required'),
+  confirmPassword: string()
+    .required('Please confirm your password')
+    .oneOf([ref('password')], 'Passwords do not match'),
+  email: string()
+    .email('Invalid email address')
+    .matches('Invalid email address')
+    .required('Email is required'),
+  name: string()
+    .min(2, 'Min 2 symbols')
+    .matches('Only letters')
+    .required('Name is required'),
+  phone: string()
+    .min(13, 'Too Short!')
+    .matches('Bad phone number')
+    .required('Phone is required'),
+  city: string()
+    .matches('Error. Example: Brovary, Kyiv')
+    .required('City is required'),
+});
+
+const RegisterForm = () => {
+  const [isShown, setIsShown] = useState(true);
+  const [showPass, setShowPass] = useState(false);
+  const [showConfirmPass, setShowConfirmPass] = useState(false);
+  const loading = useSelector(selectIsLoading);
   const dispatch = useDispatch();
-  const [passwordType, setPasswordType] = useState('password');
-  const [passwordInput, setPasswordInput] = useState('');
-  const [nameInput, setNameInput] = useState('');
-  const [emailInput, setEmailInput] = useState('');
 
-  const handlePasswordChange = evnt => {
-    setPasswordInput(evnt.target.value);
+  const showForm = () => {
+    setIsShown(false);
   };
-  const handleNameChange = evnt => {
-    setNameInput(evnt.target.value);
-  };
-  const handleEmailChange = evnt => {
-    setEmailInput(evnt.target.value);
+  const hideForm = () => {
+    setIsShown(true);
   };
 
-  const togglePassword = e => {
-    if (passwordType === 'password') {
-      setPasswordType('text');
-      return;
-    }
-    setPasswordType('password');
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
+  const onSubmit = values => {
+    const { name, email, password, phone, city } = values;
     dispatch(
-      authSignUpUser({
-        nickName: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
+      register({
+        name,
+        email,
+        password,
+        phone,
+        city,
       }),
+      hideForm()
     );
-    form.reset();
-  };
 
+  };
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      phone: '',
+      city: '',
+    },
+    validationSchema: registerSchema,
+    onSubmit,
+  });
+
+  const isValid =
+    (formik.errors.email && formik.touched.email) ||
+    (formik.errors.password && formik.touched.password) ||
+    (formik.errors.confirmPassword && formik.touched.confirmPassword) ||
+    formik.values.email === '' ||
+    formik.values.confirmPassword === ''
+      ? true
+      : false;
+
+  const showPassword = () => {
+    setShowPass(!showPass);
+  };
+  const showConfirmPassword = () => {
+    setShowConfirmPass(!showConfirmPass);
+  };
   return (
     <>
-      <form className={css.signIn__Container} onSubmit={handleSubmit}>
-        <div className={css.form__container}>
-          <p className={css.formTitle}>Sign up</p>
-          <div className={css.flexrow}>
-            <label className={css.title__label}>
-              <div>
-                Your Name
-                <div className={css.item__container}>
-                  <FaUser size="25" />
-                  <input
-                    className={css.input_active}
-                    name="name"
-                    type="text"
-                    onChange={handleNameChange}
-                    value={nameInput}
-                  />
-                </div>
-              </div>
-            </label>
-          </div>
-          <div className={css.flexrow}>
-            <label className={css.title__label}>
-              <div>
-                Your Email
-                <div className={css.item__container}>
-                  <FaEnvelope size="25" />
-                  <input
-                    className={css.input_active}
-                    name="email"
-                    type="email"
-                    onChange={handleEmailChange}
-                    value={emailInput}
-                  />
-                </div>
-              </div>
-            </label>
-          </div>
-          <div className={css.flexrow}>
-            <label className={css.title__label}>
-              <div>
-                Password
-                <div className={css.item__container}>
-                  {passwordType === 'password' ? (
-                    <FaLock size="25" onClick={togglePassword} />
-                  ) : (
-                    <FaUnlockAlt size="25" onClick={togglePassword} />
-                  )}
-                  <input
-                    className={css.input_active}
-                    name="password"
-                    type={passwordType}
-                    onChange={handlePasswordChange}
-                    value={passwordInput}
-                  />
-                </div>
-              </div>
-            </label>
-          </div>
-          <button type="submit" className={css.form_submit}>
-            Sign up
-          </button>
-        </div>
-      </form>
+      {loading ? (
+        <SpinerWrapper>
+          <Spinner />
+        </SpinerWrapper>
+      ) : (
+        <FormContainer>
+          <Formik validationSchema={registerSchema}>
+            <FormRegister onSubmit={formik.handleSubmit} autoComplete="off">
+              <Title>Register</Title>
+              {isShown && (
+                <>
+                  <div>
+                    <Input
+                      name="email"
+                      type="email"
+                      placeholder="Email"
+                      validate={registerSchema.email}
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                      onBlur={formik.handleBlur}
+                    />
+
+                    {formik.errors.email || formik.touched.email ? (
+                      <ErrBox>{formik.errors.email}</ErrBox>
+                    ) : null}
+                  </div>
+                </>
+              )}
+
+              {isShown && (
+                <>
+                  <div>
+                    <Input
+                      name="password"
+                      type={showPass ? 'text' : 'password'}
+                      placeholder="Password"
+                      onChange={formik.handleChange}
+                      value={formik.values.password}
+                      onBlur={formik.handleBlur}
+                    />
+
+                    <ShowPassword onClick={showPassword}>
+                      {!showPass ? <ImEyeBlocked /> : <ImEye />}
+                    </ShowPassword>
+                    {formik.errors.password && formik.touched.password ? (
+                      <ErrBox>{formik.errors.password}</ErrBox>
+                    ) : null}
+                  </div>
+                </>
+              )}
+              {isShown && (
+                <>
+                  <div>
+                    <Input
+                      name="confirmPassword"
+                      type={showConfirmPass ? 'text' : 'password'}
+                      placeholder="Confirm Password"
+                      onChange={formik.handleChange}
+                      value={formik.values.confirmPassword}
+                      onBlur={formik.handleBlur}
+                    />
+                    <ShowPassword onClick={showConfirmPassword}>
+                      {!showConfirmPass ? <ImEyeBlocked /> : <ImEye />}
+                    </ShowPassword>
+                    {formik.errors.confirmPassword &&
+                    formik.touched.confirmPassword ? (
+                      <ErrBox>{formik.errors.confirmPassword}</ErrBox>
+                    ) : null}
+                  </div>
+                </>
+              )}
+              {isShown && (
+                <Button type="button" onClick={showForm} disabled={isValid}>
+                  Next
+                </Button>
+              )}
+              {!isShown && (
+                <>
+                  <div>
+                    <Input
+                      name="name"
+                      type="text"
+                      placeholder="Name"
+                      onChange={formik.handleChange}
+                      value={formik.values.name}
+                      onBlur={formik.handleBlur}
+                    />
+
+                    {formik.errors.name && formik.touched.name ? (
+                      <ErrBox>{formik.errors.name}</ErrBox>
+                    ) : null}
+                  </div>
+                </>
+              )}
+              {!isShown && (
+                <>
+                  <div>
+                    <Input
+                      name="city"
+                      type="text"
+                      placeholder="City, region"
+                      onChange={formik.handleChange}
+                      value={formik.values.city}
+                      onBlur={formik.handleBlur}
+                    />
+
+                    {formik.errors.city && formik.touched.city ? (
+                      <ErrBox>{formik.errors.city}</ErrBox>
+                    ) : null}
+                  </div>
+                </>
+              )}
+              {!isShown && (
+                <>
+                  <div>
+                    <PhoneInput
+                      mask={phoneNumberMask}
+                      id="phone"
+                      type="phone"
+                      placeholder="Mobile phone"
+                      onChange={formik.handleChange}
+                      value={formik.values.phone}
+                      onBlur={formik.handleBlur}
+                      name="phone"
+                    />
+
+                    {formik.errors.phone && formik.touched.phone ? (
+                      <ErrBox>{formik.errors.phone}</ErrBox>
+                    ) : null}
+                  </div>
+                </>
+              )}
+              {!isShown && <Button type="submit">Register</Button>}
+              {!isShown && (
+                <BackButton type="button" onClick={hideForm}>
+                  Back
+                </BackButton>
+              )}
+              <BoxText>
+                <span>Already have an account?</span>{' '}
+                <StyledLink to="/login">Login</StyledLink>
+              </BoxText>
+            </FormRegister>
+          </Formik>
+          {/* <Background></Background> */}
+        </FormContainer>
+      )}
     </>
   );
-}
+};
 
-export default Register;
+export default RegisterForm;
