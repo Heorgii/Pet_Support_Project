c54c760f0bb103c184e67685f30bc367371a5c7a
JuliaGolban <julia.j.shcherban@gmail.com>
2023-03-24 17:21:50 +0200

    fix common

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,23 +1,16 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
-import { PageContainer } from './SharedLayout.styled';
 
 export const SharedLayout = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <PageContainer>
-        <Header />
+    <>
+      <Header />
+      <main>
         <Suspense fallback={null}>
           <Outlet />
         </Suspense>
-      </PageContainer>
-    </div>
+      </main>
+    </>
   );
 };
--- src/components/helpers/Scroll/ScrollToTop.jsx
+++ src/components/helpers/Scroll/ScrollToTop.jsx
@@ -1,31 +1,28 @@
-// import { useState } from 'react';
 import { BsArrowUpCircleFill } from 'react-icons/bs';
 import { ScrollBtn } from './ScrollToTop.styled';
 
 const ScrollToTop = () => {
-  // const [isScroll, setIsScroll] = useState(false);
-
-  // const scrollBy = () => {
-  //   const { height: cardHeight } = document
-  //     .querySelector('.list')
-  //     .firstElementChild.getBoundingClientRect();
-
-  //   return window.scrollBy({
-  //     top: cardHeight * 2,
-  //     behavior: 'smooth',
-  //   });
-  // };
-
-  // const scrollToTop = () => {
-  //   const start = document.querySelector('#header');
-  //   return window.scrollTo({ top: start, behavior: 'smooth' });
-  // };
+  const handleScrollToTop = () => {
+    const start = document.querySelector('#header');
+    return window.scrollTo({ top: start, behavior: 'smooth' });
+  };
 
   return (
-    <ScrollBtn type="button" aria-label="Arrow up" onClick={this.scrollToTop}>
+    <ScrollBtn type="button" aria-label="Arrow up" onClick={handleScrollToTop}>
       <BsArrowUpCircleFill size={30} />
     </ScrollBtn>
   );
 };
 
-export default ScrollToTop;
+const ScrollBy = () => {
+  const { height: cardHeight } = document
+    .querySelector('list')
+    .firstElementChild.getBoundingClientRect();
+
+  return window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+};
+
+export { ScrollToTop, ScrollBy };
