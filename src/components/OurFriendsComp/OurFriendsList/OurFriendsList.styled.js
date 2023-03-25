f3299eda2e79d87659036184e983fcda923fb91d
JuliaGolban <julia.j.shcherban@gmail.com>
2023-03-25 21:06:15 +0200

    fix: change row height

--- src/components/OurFriendsComp/OurFriendsList/OurFriendsList.styled.js
+++ src/components/OurFriendsComp/OurFriendsList/OurFriendsList.styled.js
@@ -4,19 +4,21 @@ const List = styled.ul`
   display: grid;
   max-width: calc(100vw - 40px);
   grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  grid-template-rows: 1fr;
   grid-gap: 16px;
   padding: 0;
 
   @media screen and (min-width: 768px) {
     max-width: calc(100vw - 34px);
     grid-template-columns: repeat(auto-fill, minmax(336px, 1fr));
-    grid-auto-rows: auto;
+    /* grid-auto-rows: auto; */
+    grid-template-rows: 1fr 1fr 1fr;
     grid-gap: 32px;
   }
 
   @media screen and (min-width: 1280px) {
     max-width: 1280px;
-    grid-template-columns: repeat(auto-fill, minmax(390px, 1fr));
+    /* grid-template-columns: repeat(auto-fill, minmax(390px, 1fr)); */
   }
 `;
 
