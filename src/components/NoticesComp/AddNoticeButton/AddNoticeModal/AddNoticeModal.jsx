287ab088a6fb36416fe6e8c0830acb372c8c6e37
RudenkoSerhii <rudenko.sergey@ukr.net>
2023-03-26 18:31:01 +0300

    second

--- src/components/NoticesComp/AddNoticeButton/AddNoticeModal/AddNoticeModal.jsx
+++ src/components/NoticesComp/AddNoticeButton/AddNoticeModal/AddNoticeModal.jsx
@@ -96,6 +96,52 @@ function changeInput(setFieldValue) {
 
 }
 
+//   const {
+//     ready,
+//     value,
+//     suggestions: { status, data },
+//     setValue,
+//     clearSuggestions,
+//   } = usePlacesAutocomplete({
+//     callbackName: "callback",
+//     requestOptions: {
+
+//     },
+//     debounce: 300,
+//   });
+//   const ref = useOnclickOutside(() => {
+//     clearSuggestions();
+//   });
+
+//   const handleInput = (e) => {
+// console.log(e.target.value)
+//     setValue(e.target.value);
+//   };
+
+//   const handleSelect =
+//     ({ description }) =>
+//     () => {
+
+//       setValue(description, false);
+//       clearSuggestions();
+
+//     };
+
+//   const renderSuggestions = () =>
+//     data.map((suggestion) => {
+//       const {
+//         place_id,
+//         structured_formatting: { main_text, secondary_text },
+//       } = suggestion;
+
+//       return (
+//         <li key={place_id} onClick={handleSelect(suggestion)}>
+//           <strong>{main_text}</strong> <small>{secondary_text}</small>
+//         </li>
+//       );
+//     });
+
+
   return ReactDOM.createPortal(
     Object.values(modal)[0] === 'formSell' && (
       <Overlay onClick={e => onClickBackdrop(e)}>
