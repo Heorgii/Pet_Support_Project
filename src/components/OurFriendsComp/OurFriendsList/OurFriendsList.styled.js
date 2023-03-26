098effcdfc23756cc531765e3b933920030134be
JuliaGolban <julia.j.shcherban@gmail.com>
2023-03-26 19:12:45 +0300

    change grid friends

--- src/components/OurFriendsComp/OurFriendsList/OurFriendsList.styled.js
+++ src/components/OurFriendsComp/OurFriendsList/OurFriendsList.styled.js
@@ -1,24 +1,38 @@
 import styled from 'styled-components';
 
 const List = styled.ul`
-  display: grid;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: stretch;
+  gap: 16px;
   max-width: calc(100vw - 40px);
+
+  /* display: -ms-grid;
+  display: grid;
   grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  -ms-grid-columns: repeat(auto-fill, minmax(280px, 1fr));
   grid-template-rows: 1fr;
-  grid-gap: 16px;
-  padding: 0;
+  grid-auto-rows: 1fr;
+  grid-gap: 16px; */
 
   @media screen and (min-width: 768px) {
-    max-width: calc(100vw - 34px);
-    grid-template-columns: repeat(auto-fill, minmax(336px, 1fr));
-    /* grid-auto-rows: auto; */
-    grid-template-rows: 1fr 1fr 1fr;
-    grid-gap: 32px;
+    gap: 32px;
+    max-width: calc(100vw - 64px);
+
+    /* grid-template-columns: repeat(auto-fit, minmax(336px, 1fr));
+    -ms-grid-columns: repeat(auto-fit, minmax(336px, 1fr));
+    grid-auto-rows: auto;
+    -ms-grid-rows: auto;
+    grid-gap: 32px; */
   }
 
   @media screen and (min-width: 1280px) {
+    justify-content: space-between;
     max-width: 1280px;
-    /* grid-template-columns: repeat(auto-fill, minmax(390px, 1fr)); */
+
+    /* grid-template-columns: repeat(auto-fill, minmax(390px, 1fr));
+    -ms-grid-columns: repeat(auto-fill, minmax(390px, 1fr)); */
   }
 `;
 
