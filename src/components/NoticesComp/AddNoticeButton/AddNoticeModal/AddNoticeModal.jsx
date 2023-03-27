572e8df6b3f8fb5191d404785e125c6c8611c5fc
RudenkoSerhii <rudenko.sergey@ukr.net>
2023-03-27 18:24:39 +0300

    add image format & size check

--- src/components/NoticesComp/AddNoticeButton/AddNoticeModal/AddNoticeModal.jsx
+++ src/components/NoticesComp/AddNoticeButton/AddNoticeModal/AddNoticeModal.jsx
@@ -38,7 +38,7 @@ import schemas from 'components/Schemas/schemas';
 import { useState } from 'react';
 import { fetchNotice } from 'services/APIservice';
 import { onLoading, onLoaded } from 'components/helpers/Loader/Loader';
-import { onFetchError } from 'components/helpers/Messages/NotifyMessages';
+import { onFetchError, onInfo } from 'components/helpers/Messages/NotifyMessages';
 import usePlacesAutocomplete from 'use-places-autocomplete';
 import useOnclickOutside from 'react-cool-onclickoutside';
 import { breedsValue } from 'redux/breeds/selectors';
@@ -67,6 +67,18 @@ export const AddNoticeModal = () => {
 
   function setImage(e) {
     const input = document.querySelector('.file');
+if(input.files[0] && input.files[0].size >= 2048000) {
+input.value = '';
+e.target.style = '';
+onInfo('File size must be less than 2Mb!')
+return
+}
+if(input.files[0] && !['jpeg', 'png', 'webp', 'gif', 'jpg'].includes((input.files[0].type).split('/')[1])) {
+input.value = '';
+e.target.style = '';
+onInfo('File type must be only image!')
+return
+}
     const reader = new FileReader();
     e.target.style = '';
 
