0e953945e4f5da877e1b8d185adec98e55faf4ab
PavelLesovoy <98907227+PavelLesovoy@users.noreply.github.com>
2023-03-29 22:41:54 +0300

    RegExp fix

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = `https://petsapi.cyclic.app/api`;
+// axios.defaults.baseURL = `https://petsapi.cyclic.app/api`;
+axios.defaults.baseURL = `http://localhost:3030/api`;
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
