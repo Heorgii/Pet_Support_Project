03ef7d10c3ce21513785cd822b172c6fdf051807
Heorgii <rushchakheorgii@gmail.com>
2023-03-29 09:04:01 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { lazy, useEffect } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';//Navigate
 import { RestrictedRoute } from 'routes/RestrictedRoute';
 import { PrivateRoute } from 'routes/PrivateRoute';
 import { SharedLayout } from '../SharedLayout/SharedLayout';
