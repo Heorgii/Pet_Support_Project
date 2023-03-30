910612d6f2193ef11ca3e00bd831fbf3479b31b6
RudenkoSerhii <rudenko.sergey@ukr.net>
2023-03-30 23:36:19 +0300

    fix setimage

--- src/utils/setimage.js
+++ src/utils/setimage.js
@@ -1,34 +1,31 @@
 import { onInfo } from 'components/helpers/Messages/NotifyMessages';
 
-
-export function setImage(e, setFieldValues) {
-    const input = document.querySelector('.file');
-    if (input.files[0] && input.files[0].size >= 2048000) {
-      input.value = '';
-      e.target.style = '';
-      onInfo('File size must be less than 2Mb!');
-      return;
-    }
-    if (
-      input.files[0] &&
-      !['jpeg', 'png', 'webp', 'gif', 'jpg'].includes(
-        input.files[0].type.split('/')[1],
-      )
-    ) {
-      input.value = '';
-      e.target.style = '';
-      onInfo('File type must be only image!');
-      return;
-    }
-    const reader = new FileReader();
+export function setImage(e) {
+  const input = document.querySelector('.file');
+  if (input.files[0] && input.files[0].size >= 2048000) {
+    input.value = '';
     e.target.style = '';
-
-    reader.onload = function () {
-      setFieldValues(
-        e.target.style = `background-image: url(${reader.result}); background-size: contain; background-position: center; background-repeat: no-repeat;`
-      )
-    };
-    if (input.files[0]) {
-      reader.readAsDataURL(input.files[0]);
-    }
+    onInfo('File size must be less than 2Mb!');
+    return;
   }
+  if (
+    input.files[0] &&
+    !['jpeg', 'png', 'webp', 'gif', 'jpg'].includes(
+      input.files[0].type.split('/')[1],
+    )
+  ) {
+    input.value = '';
+    e.target.style = '';
+    onInfo('File type must be only image!');
+    return;
+  }
+  const reader = new FileReader();
+  e.target.style = '';
+
+  reader.onload = function () {
+    e.target.style = `background-image: url(${reader.result}); background-size: contain; background-position: center; background-repeat: no-repeat;`;
+  };
+  if (input.files[0]) {
+    reader.readAsDataURL(input.files[0]);
+  }
+}
