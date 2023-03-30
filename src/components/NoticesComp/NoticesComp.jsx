ef4fa05d086ea0de87ffc6ebe692d884e756ada8
JuliaGolban <julia.j.shcherban@gmail.com>
2023-03-30 20:00:09 +0300

    fix notice error

--- src/components/NoticesComp/NoticesComp.jsx
+++ src/components/NoticesComp/NoticesComp.jsx
@@ -4,12 +4,14 @@ import { AddNoticeButton } from './AddNoticeButton/AddNoticeButton';
 import { NoticesCategoriesList } from './NoticesCategoriesList/NoticesCategoriesList';
 import { WrapperNav } from './NoticesComp.styled';
 import { useState } from 'react';
+import { Title } from 'components/baseStyles/CommonStyle.styled';
 
 export const NoticesComp = () => {
   const [total, setTotal] = useState(0);
 
   return (
     <>
+      <Title>Find your favorite pet</Title>
       <NoticesSearch />
       <WrapperNav>
         <NoticesCategoriesNav />
--- src/components/NoticesComp/NoticesSearch/NoticesSearch.jsx
+++ src/components/NoticesComp/NoticesSearch/NoticesSearch.jsx
@@ -7,7 +7,7 @@ import {
   LabelStyled,
   IconSearch,
 } from './NoticesSearch.styled';
-import { Title } from 'components/baseStyles/CommonStyle.styled';
+
 import { onInfo } from 'components/helpers/Messages/NotifyMessages';
 import { useDispatch } from 'react-redux';
 import { addQuery } from 'redux/query/slice';
@@ -17,56 +17,52 @@ export const NoticesSearch = () => {
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <Title>Find your favorite pet</Title>
-
-      <Formik
-        initialValues={{ search: '' }}
-        onSubmit={(values, actions) => {
-          if (values.search === '') {
-            onInfo('Fill the field!');
-            actions.setSubmitting(false);
-          } else {
-            dispatch(addQuery(values.search));
-            dispatch(addPage(1));
-            actions.setSubmitting(false);
-          }
-        }}
-      >
-        {({ isSubmitting, values, handleSubmit, handleChange }) => (
-          <FormStyled onSubmit={handleSubmit} autoComplete="off">
-            <LabelStyled>
-              <FieldStyled
-                id="search"
-                type="search"
-                name="search"
-                placeholder="Search"
-                value={values.search}
-                onChange={e => {
-                  handleChange(e);
-                  document
-                    .querySelector('#search')
-                    .addEventListener('input', e => {
-                      if (e.target.value === '') {
-                        dispatch(addPage(1));
-                        dispatch(addQuery(''));
-                      }
-                    });
-                }}
-              />
-              <div>
-                <ButtonStyled
-                  type="submit"
-                  disabled={isSubmitting}
-                  onSubmit={handleSubmit}
-                >
-                  <IconSearch />
-                </ButtonStyled>
-              </div>
-            </LabelStyled>
-          </FormStyled>
-        )}
-      </Formik>
-    </div>
+    <Formik
+      initialValues={{ search: '' }}
+      onSubmit={(values, actions) => {
+        if (values.search === '') {
+          onInfo('Fill the field!');
+          actions.setSubmitting(false);
+        } else {
+          dispatch(addQuery(values.search));
+          dispatch(addPage(1));
+          actions.setSubmitting(false);
+        }
+      }}
+    >
+      {({ isSubmitting, values, handleSubmit, handleChange }) => (
+        <FormStyled onSubmit={handleSubmit} autoComplete="off">
+          <LabelStyled>
+            <FieldStyled
+              id="search"
+              type="search"
+              name="search"
+              placeholder="Search"
+              value={values.search}
+              onChange={e => {
+                handleChange(e);
+                document
+                  .querySelector('#search')
+                  .addEventListener('input', e => {
+                    if (e.target.value === '') {
+                      dispatch(addPage(1));
+                      dispatch(addQuery(''));
+                    }
+                  });
+              }}
+            />
+            <div>
+              <ButtonStyled
+                type="submit"
+                disabled={isSubmitting}
+                onSubmit={handleSubmit}
+              >
+                <IconSearch />
+              </ButtonStyled>
+            </div>
+          </LabelStyled>
+        </FormStyled>
+      )}
+    </Formik>
   );
 };
